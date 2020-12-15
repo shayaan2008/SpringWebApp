a05bd62db6bdd71b6b6ff39c37e368767c118d1c
mdmasudul <masud_3000@yahoo.com>
2020-12-15 15:28:35 -0500

    First Commit

---
+++ src/test/java/com/masud/secureapp/SecureAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.masud.secureapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
